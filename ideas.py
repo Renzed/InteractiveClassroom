b33d35b6481e9c5ff975d41ca96b2ac343077dec
renzed <guitarenze@hotmail.nl>
2025-05-02 11:33:52 +0200

    Idea view with removal option

--- ideas.py
+++ ideas.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
+def remove_idea(id):
+    st.session_state[f'box{id}'] = False
+    st.session_state['ideas'].remove(id)
+
 for i, id in enumerate(st.session_state['ideas']):
     with st.container(border=True):
-        st.subheader(f"Idea {i+1}", divider=True)
-        st.write(st.session_state.messages[id]["content"])
+        col1, col2 = st.columns([8,1])
+        with col1:
+            st.subheader(f"Idea {i+1}", divider=True)
+            st.write(st.session_state.messages[id]["content"])
+        with col2:
+            st.button("", key=f"button{i}", on_click=remove_idea, args=(id,), icon=":material/delete:", use_container_width=True)
